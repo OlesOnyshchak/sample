c328ad9b634918ea2f3e9a31fa22231056d5e31f
OlesOnyshchak <oles1719@gmail.com>
2016-09-27 22:58:05 +0300

    add jsp

---
+++ src/main/java/com/sample/general/servlet/MyBean.java
@@ -0,0 +1,15 @@
+package com.sample.general.servlet;
+
+/**
+ * Created by Oles on 9/27/2016.
+ */
+public class MyBean {
+    private String name;
+    public MyBean(){
+        name = "oles";
+    }
+
+    public String getName() {
+        return name;
+    }
+}
